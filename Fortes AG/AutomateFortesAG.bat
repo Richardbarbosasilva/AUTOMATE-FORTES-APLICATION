89f849390786b5b3cc4fcd42d25c9d1453bc8950
Richard Barbosa Silva <112967185+Richardbarbosasilva@users.noreply.github.com>
2023-12-14 15:38:13 -0400

    Update and rename AutomateFortesAC.bat to AutomateFortesAG.bat and adjusts commentary, also putting them on english, they were in portuguese before

--- Fortes AG/AutomateFortesAG.bat
+++ Fortes AG/AutomateFortesAG.bat
@@ -2,42 +2,42 @@
 
 ::create some variables to tell the batch file where to find the powershell script.
 
-set "caminhodownloads=%USERPROFILE%\Downloads\automatefortesac.ps1"
-set "caminhoareadetrabalho=%USERPROFILE%\Desktop\automatefortesac.ps1"
-set "caminhodocumentos=%USERPROFILE%\Documents\automatefortesac.ps1"
-set "caminhopastafortes=C:\Fortes\AC\automateFortesac.ps1"
-set "caminhopastaatual=%~dp0automatefortesac.ps1"
+set "caminhodownloads=%USERPROFILE%\Downloads\automatefortesag.ps1"
+set "caminhoareadetrabalho=%USERPROFILE%\Desktop\automatefortesag.ps1"
+set "caminhodocumentos=%USERPROFILE%\Documents\automatefortesag.ps1"
+set "caminhopastafortes=C:\Fortes\AC\automateFortesag.ps1"
+set "caminhopastaatual=%~dp0automatefortesag.ps1"
 
 :: Verify if the file does exists
 
 if exist "%caminhodownloads%" (
     echo Arquivo encontrado. Iniciando PowerShell.
-    :: Comando para executar o script em modo admin e burlar a política de execução do PowerShell
+    :: Bypasses powershell's policy execution, executes it with admin privileges and close the powershell's window after concludes the process.
     PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0automatefortesac.ps1\"' -Verb RunAs}"
 ) else if exist "%caminhoareadetrabalho%" (
     echo Arquivo encontrado. Iniciando PowerShell.
-    :: Comando para executar o script em modo admin e burlar a política de execução do PowerShell
+     :: Bypasses powershell's policy execution, executes it with admin privileges and close the powershell's window after concludes the process.
   PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0automatefortesac.ps1\"' -Verb RunAs}"
 ) else if exist "%caminhodocumentos%" (
     echo Arquivo encontrado. Iniciando PowerShell.
-    :: Comando para executar o script em modo admin e burlar a política de execução do PowerShell
+    :: Bypasses powershell's policy execution, executes it with admin privileges and close the powershell's window after concludes the process.
     PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0automatefortesac.ps1\"' -Verb RunAs}"
 ) else if exist "%caminhopastafortes%" (
     echo Arquivo encontrado. Iniciando PowerShell.
-    :: Comando para executar o script em modo admin e burlar a política de execução do PowerShell
+    :: Bypasses powershell's policy execution, executes it with admin privileges and close the powershell's window after concludes the process.
    PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0automatefortesac.ps1\"' -Verb RunAs}"
 ) else if exist "%caminhopastaatual%" (    
    echo Arquivo encontrado. Iniciando PowerShell.
-    :: Comando para executar o script em modo admin e burlar a política de execução do PowerShell
+    :: Bypasses powershell's policy execution, executes it with admin privileges and close the powershell's window after concludes the process.
     PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0automatefortesac.ps1\"' -Verb RunAs}"
 ) else (
-    goto :ArquivoNaoEncontrado
+    goto :Filenotfound
 )
 
-:ArquivoNaoEncontrado
+:Filenotfound
 
 echo Script PowerShell automatefortesag.ps1 not found, verify it's path and try once again.
 
-:: exit
+:: exit or pause
 
-exit -force
+exit 
