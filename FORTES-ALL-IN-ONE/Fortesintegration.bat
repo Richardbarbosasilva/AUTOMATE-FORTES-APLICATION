61cdcddecb73db2dd67b425b3f9ce24ef8d04e37
Richard Barbosa Silva <112967185+Richardbarbosasilva@users.noreply.github.com>
2023-12-19 08:42:24 -0400

    Update Fortesintegration.bat
    This script searchs and executes the following powershell script to automate Fortes atualization

--- FORTES-ALL-IN-ONE/Fortesintegration.bat
+++ FORTES-ALL-IN-ONE/Fortesintegration.bat
@@ -1,1 +1,44 @@
+@echo off
+
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
+
+:: Criar as variáveis responsáveis por identificar o caminho do script PowerShell nos principais pontos de salvamento de arquivos do PC do usuário 
+
+set "caminhodownloads=%USERPROFILE%\Downloads\INTEGRACAOFORTES.ps1"
+set "caminhoareadetrabalho=%USERPROFILE%\Desktop\INTEGRACAOFORTES.ps1"
+set "caminhodocumentos=%USERPROFILE%\Documents\INTEGRACAOFORTES.ps1"
+set "caminhopastafortes=C:\Fortes\AC\INTEGRACAOFORTES.ps1"
+set "caminhopastaatual=%~dp0INTEGRACAOFORTES.ps1"
+
+:: Verificar se o arquivo existe
+if exist "%caminhodownloads%" (
+    echo Arquivo encontrado. Iniciando PowerShell minimizado.
+    :: Comando para executar o script em modo admin e burlar a política de execução do PowerShell
+    powershell.exe -WindowStyle Minimized -NoProfile -ExecutionPolicy Bypass -File "%~dp0INTEGRACAOFORTES.ps1" -Verb RunAs
+) else if exist "%caminhoareadetrabalho%" (
+    echo Arquivo encontrado. Iniciando PowerShell minimizado.
+    :: Comando para executar o script em modo admin e burlar a política de execução do PowerShell
+    powershell.exe -WindowStyle Minimized -NoProfile -ExecutionPolicy Bypass -File "%~dp0INTEGRACAOFORTES.ps1" -Verb RunAs
+) else if exist "%caminhodocumentos%" (
+    echo Arquivo encontrado. Iniciando PowerShell minimizado.
+    :: Comando para executar o script em modo admin e burlar a política de execução do PowerShell
+    powershell.exe -WindowStyle Minimized -NoProfile -ExecutionPolicy Bypass -File "%~dp0INTEGRACAOFORTES.ps1" -Verb RunAs
+) else if exist "%caminhopastafortes%" (
+    echo Arquivo encontrado. Iniciando PowerShell minimizado.
+    :: Comando para executar o script em modo admin e burlar a política de execução do PowerShell
+    powershell.exe -WindowStyle Minimized -NoProfile -ExecutionPolicy Bypass -File "%~dp0INTEGRACAOFORTES.ps1" -Verb RunAs
+) else if exist "%caminhopastaatual%" (    
+    echo Arquivo encontrado. Iniciando PowerShell minimizado.
+    :: Comando para executar o script em modo admin e burlar a política de execução do PowerShell
+    powershell.exe -WindowStyle Minimized -NoProfile -ExecutionPolicy Bypass -File "%~dp0INTEGRACAOFORTES.ps1" -Verb RunAs
+) else (
+    goto :ArquivoNaoEncontrado 
+)
+
+:ArquivoNaoEncontrado
+echo Script PowerShell INTEGRACAOFORTES.ps1 not found, verify its path and try once again.
+
+:: exit
+exit
+
 
